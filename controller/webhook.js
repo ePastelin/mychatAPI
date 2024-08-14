47a858916a3dbd543c4f71ecb05e84bbcedbc5ae
azulcolor <lealpastelin@gmail.com>
2024-08-14 17:06:47 -0500

    all information

--- controller/webhook.js
+++ controller/webhook.js
@@ -1,21 +1,23 @@
 
 import { pool } from '../database/config.js';
 import { wss } from '../index.js'
+import 'dotenv/config';
 
-export const verificar = (req, res) => {
-    try {
-        var tokenandercode = "TOKENMETA";
-        var token = req.query["hub.verify_token"];
-        var challenge = req.query["hub.challenge"];
+const { WEBHOOK_VERIFY_TOKEN } = process.env;
 
-        if (challenge != null && token != null && token == tokenandercode) {
-            res.send(challenge);
-        } else {
-            res.status(400).send();
-        }
-    } catch (e) {
-        console.error('Error en la verificación:', e);
-        res.status(400).send();
+export const verificar = async (req, res) => {
+    const mode = req.query["hub.mode"];
+    const token = req.query["hub.verify_token"];
+    const challenge = req.query["hub.challenge"];
+  
+    // check the mode and token sent are correct
+    if (mode === "subscribe" && token === WEBHOOK_VERIFY_TOKEN) {
+      // respond with 200 OK and challenge token from the request
+      res.status(200).send(challenge);
+      console.log("Webhook verified successfully!");
+    } else {
+      // respond with '403 Forbidden' if verify tokens do not match
+      res.sendStatus(403);
     }
 };
 
