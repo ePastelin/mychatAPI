b959504518bc9b9c9bba17225769efc99922eba3
azulcolor <lealpastelin@gmail.com>
2024-08-14 17:12:42 -0500

    no database

--- database/config.js
+++ database/config.js
@@ -1,24 +1,24 @@
-import { createPool } from "mysql2/promise";
-import 'dotenv/config';
+// import { createPool } from "mysql2/promise";
+// import 'dotenv/config';
 
 
-const { DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE, DB_PORT } = process.env;
+// const { DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE, DB_PORT } = process.env;
 
-export const pool = createPool({
-    host: DB_HOST,
-    user: DB_USER,
-    password: DB_PASSWORD,
-    database: DB_DATABASE,
-    port: DB_PORT,
-})
+// export const pool = createPool({
+//     host: DB_HOST,
+//     user: DB_USER,
+//     password: DB_PASSWORD,
+//     database: DB_DATABASE,
+//     port: DB_PORT,
+// })
 
-export const getConnection = async () => {
-    try {
-        const connection = await pool.getConnection();
-        console.log('Database connected');
-        connection.release(); // Release the connection back to the pool
-    }catch (error) {
-        console.log(error);
-        throw new Error('Database connection failed');
-    }
-}
+// export const getConnection = async () => {
+//     try {
+//         const connection = await pool.getConnection();
+//         console.log('Database connected');
+//         connection.release(); // Release the connection back to the pool
+//     }catch (error) {
+//         console.log(error);
+//         throw new Error('Database connection failed');
+//     }
+// }
