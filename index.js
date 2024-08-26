2a878066dd523f2a49e6d51a12ea3047e7650048
azulcolor <lealpastelin@gmail.com>
2024-08-26 11:22:42 -0500

    cleanest index

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 import express from 'express';
 import { createServer } from 'http';
-import webhook from './routes/webhook.routes.js';
-import chat from './routes/chat.routes.js';
-import auth from './routes/auth.routes.js';
 import { getConnection } from './database/config.js';
 import cors from 'cors';
 import setupWebSocket from './services/websocket.js';
@@ -21,12 +18,7 @@ getConnection();
 app.use(cors());
 app.use(express.json());
 
-
-app.use("/api", webhook);
-app.use('/api/chat', chat);
-app.use('/api/auth', auth);
-
-
+app.use(routes)
 
 server.listen(PORT, () => {
     console.log("El servidor está escuchando en el puerto: " + PORT);
