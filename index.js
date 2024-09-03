ac5fa59a9b4edf81ea0a8a8f76022cbcffd10d32
azulcolor <lealpastelin@gmail.com>
2024-09-03 11:55:02 -0500

    pool inyection

--- index.js
+++ index.js
@@ -4,13 +4,14 @@ import { getConnection } from './database/config.js';
 import cors from 'cors';
 import setupWebSocket from './services/websocket.js';
 import routes from './routes/index.js'
+import { pool } from './database/config.js';
 
 const app = express();
 const server = createServer(app);  // Combina el servidor HTTP y Express
 const PORT = process.env.PORT || 3000;
 
 // Inicializa el WebSocket Server
-export const wss = setupWebSocket(server)
+export const wss = setupWebSocket(server, pool)
 
 // Database connection
 getConnection();
