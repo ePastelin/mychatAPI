01bf10901278d759de8d78863c61b9b44dd2634c
azulcolor <lealpastelin@gmail.com>
2024-09-17 15:54:52 -0500

    tryng to create a template

--- controller/template.js
+++ controller/template.js
@@ -1,3 +1,12 @@
-export const createTemplate = (req, res) => {
+import api from "../helpers/axios"
 
+export const createTemplate = async (req, res) => {
+
+    const { body } = req
+
+    console.log(body)
+    
+    const response = await api.post(url, body)
+
+    res.status(200).json(response)
 }
