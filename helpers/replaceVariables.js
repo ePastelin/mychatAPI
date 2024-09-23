dc466054066dec8942290866cee50d4eae3a3734
azulcolor <lealpastelin@gmail.com>
2024-09-23 15:22:08 -0500

    debug

--- helpers/replaceVariables.js
+++ helpers/replaceVariables.js
@@ -1,5 +1,6 @@
 export default function replaceVariables(text, exampleArray) {
     let result = text;
+    console.log(text, exampleArray)
     exampleArray.forEach((value, index) => {
         const placeholder = `{{${index + 1}}}`; // {{1}}, {{2}}, etc.
         result = result.replace(placeholder, value);
