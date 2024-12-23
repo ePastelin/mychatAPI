234901fbeed083780bc2b1e30f8ceb8c8d53a65e
azulcolor <lealpastelin@gmail.com>
2024-12-23 16:58:13 -0500

    testing gpt api

--- services/chatbot/gpt.js
+++ services/chatbot/gpt.js
@@ -8,9 +8,10 @@ const apiKey = process.env.OPENAI_API_KEY;
 const openai = new OpenAI({apiKey});
 
 export const gptResponse = async (userContent, idChat) => {
+    const messages = await getMessageForGPTBot(pool, idChat);
     const completion = await openai.chat.completions.create({
         model: 'gpt-4o-mini',
-        messages: await getMessageForGPTBot(pool, idChat).push({role: 'system', content: prompt}).push({role: 'user', content: userContent}) 
+        messages: messages.push({role: 'system', content: prompt}).push({role: 'user', content: userContent}) 
     })
     return completion.choices[0].message.content;
 }
