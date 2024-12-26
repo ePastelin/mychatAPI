2209ac44b94cdd65d729b6b17147b3f67037ddb9
azulcolor <lealpastelin@gmail.com>
2024-12-26 13:02:29 -0500

    production

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
 import express from "express";
-import { createServer } from "http";
-import { getConnection } from "./database/config.js";
+import https from "https";
+import http from "http";
+import fs from "fs";
 import cors from "cors";
+import path from "path";
+import { getConnection, pool } from "./database/config.js";
 import setupWebSocket from "./services/websocket.js";
 import routes from "./routes/index.js";
-import { pool } from "./database/config.js";
-import path from 'path';
 import __dirname from "./helpers/getDirname.cjs";
 
-const app = express();
-const server = createServer(app);
+console.log("NODE_ENV:", process.env.NODE_ENV);
+console.log("PORT:", process.env.PORT);
+console.log("CERT_PATH:", process.env.CERT_PATH);
+
 const PORT = process.env.PORT || 3000;
+const USE_HTTPS = process.env.USE_HTTPS === "true";
+
+const app = express();
+const server = USE_HTTPS
+  ? https.createServer(
+      {
+        cert: fs.readFileSync(process.env.CERT_PATH),
+        key: fs.readFileSync(process.env.KEY_PATH),
+        cert: fs.readFileSync(process.env.CA_PATH),
+      },
+      app
+    )
+  : http.createServer(app);
 
 export const wss = setupWebSocket(server, pool);
 
@@ -18,13 +34,10 @@ getConnection();
 
 app.use(cors());
 app.use(express.json());
-
 app.use(routes);
 
-app.use('/multimedia', express.static(path.join(__dirname, 'multimedia')));
+app.use("/multimedia", express.static(path.join(__dirname, "multimedia")));
 
-if (process.env.NODE_ENV !== "test") {
-  server.listen(PORT, () => {
-    console.log("El servidor está escuchando en el puerto: " + PORT);
-  });
-}
+server.listen(PORT, () => {
+  console.log(`Servidor ${USE_HTTPS ? "HTTPS" : "HTTP"} escuchando en el puerto ${PORT}`);
+});
